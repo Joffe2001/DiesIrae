9d00aa4a6541d2a8a70398770fd8bc489429c367
cryoxe <71180076+cryoxe@users.noreply.github.com>
2025-11-16 17:20:45 +0100

    feat(Elijah): Added utils file to create elijah's beggars

---
+++ scripts/npcs/elijah/elijah_utils_beggar.lua
@@ -0,0 +1,158 @@
+local mod = DiesIraeMod
+local game = Game()
+local sfx = SFXManager()
+
+
+--- MAGIC NUMBERS
+---
+
+local BASE_REWARD_CHANCES = 0.33
+
+
+--- Definitions
+---
+
+local elijah = mod.Players.Elijah
+
+
+---@alias statDownFun fun(data: table)
+---@class StatDownEntry
+---@field key string
+---@field func statDownFun
+---@type StatDownEntry[]
+local statsDownFuncs = {
+    {
+        key = "WillSpeed",
+        func = function(data)
+            data.WillSpeed = (data.WillSpeed or 0) - WILL_SPEED_UP
+        end
+    },
+    {
+        key = "WillFireDelay",
+        func = function(data)
+            data.WillFireDelay = (data.WillFireDelay or 0) - WILL_TEARS_UP
+        end
+    },
+    {
+        key = "WillDamage",
+        func = function(data)
+            data.WillDamage = (data.WillDamage or 0) - WILL_DAMAGE_UP
+        end
+    },
+    {
+        key = "WillRange",
+        func = function(data)
+            data.WillRange = (data.WillRange or 0) - WILL_RANGE_UP
+        end
+    },
+    {
+        key = "WillShotSpeed",
+        func = function(data)
+            data.WillShotSpeed = (data.WillShotSpeed or 0) - WILL_SHOT_SPEED_UP
+        end
+    },
+    {
+        key = "WillLuck",
+        func = function(data)
+            data.WillLuck = (data.WillLuck or 0) - WILL_LUCK_UP
+        end
+    },
+}
+
+
+local beggarFuncs = {}
+
+
+--- Functions
+---
+
+---Simulate a beggar drop
+---@param beggar EntityNPC
+---@param player EntityPlayer
+---@param rewardChance number | nil
+function beggarFuncs.OnBeggarCollision(beggar, player, rewardChance)
+    local sprite = beggar:GetSprite()
+
+    if player:GetPlayerType() ~= elijah then return end
+    if not sprite:IsPlaying("Idle") then return end
+
+    local rng = beggar:GetDropRNG()
+    local paid = beggarFuncs:DrainElijahStat(player)
+
+    if not paid then return end
+
+    sfx:Play(SoundEffect.SOUND_SCAMPER)
+
+    if rng:RandomFloat() > (rewardChance or BASE_REWARD_CHANCES) then
+        sprite:Play("PayPrize")
+        return true
+    else
+        sprite:Play("PayNothing")
+        return false
+    end
+end
+
+
+---Remove stats from an Elijah character
+---@param player EntityPlayer
+---@param rng RNG
+---@return boolean
+function beggarFuncs.DrainElijahsWill(player, rng)
+    if player:GetPlayerType() ~= elijah then return false end
+
+    local data = mod.SaveManager.GetRunSave(player)
+
+    local canGoDown = {}
+
+    for _, stat in ipairs(statsDownFuncs) do
+        if (data[stat.key] or 0) > 0 then
+            table.insert(canGoDown, stat)
+        end
+    end
+
+    if #canGoDown == 0 then return false end
+
+    local pick = canGoDown[rng:RandomInt(#canGoDown) + 1]
+    pick.fun(data)
+    return true
+end
+
+
+---Basic beggar stats machine that gives a random item from a pool then vanish
+---@param beggarEntity EntityNPC
+---@param itemPool ItemPoolType
+function beggarFuncs.StateMachine(beggarEntity, itemPool)
+    local sprite = beggarEntity:GetSprite()
+    local rng = beggarEntity:GetDropRNG()
+
+    if sprite:IsFinished("PayNothing") then
+        sprite:Play("Idle")
+    elseif sprite:IsFinished("PayPrize") then
+        sprite:Play("Prize")
+    end
+
+    if sprite:IsFinished("Prize") then
+        sfx:Play(SoundEffect.SOUND_SLOTSPAWN)
+        local item = game:GetItemPool():GetCollectible(itemPool, true, rng:Next())
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, item,
+            Isaac.GetFreeNearPosition(beggarEntity.Position, 40), Vector.Zero, nil)
+
+        sprite:Play("Teleport")
+    end
+
+    if sprite:IsFinished("Teleport") then
+        beggarEntity:Remove()
+    end
+end
+
+
+---SFX BOOM then remove the beggar
+---@param beggar EntityNPC
+function beggarFuncs.DoBeggarExplosion(beggar)
+    sfx:Play(SoundEffect.SOUND_MEATY_DEATHS)
+    game:SpawnParticles(beggar.Position, EffectVariant.BLOOD_EXPLOSION, 1, 3, Color.Default)
+    Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.BLOOD_SPLAT, 0, beggar.Position, Vector.Zero, beggar)
+    beggar:Remove()
+end
+
+return beggarFuncs
