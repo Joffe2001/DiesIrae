45f4afafb42c1eef49e09dda75f7dfb1cb15d2d0
cryoxe <71180076+cryoxe@users.noreply.github.com>
2025-11-16 03:49:13 +0100

    fix(utils): inverted args

--- scripts/core/utils.lua
+++ scripts/core/utils.lua
@@ -9,12 +9,12 @@ local utils = {}
 ---
 
 ---Correctly apply a TearsUp to a character. From Hybrid
----@param amount number
 ---@param current number
+---@param amount number
 ---@return number
-function utils.TearsUp(amount, current)
-    local currentTears = 30 / (amount + 1)
-    local newTears = currentTears + current
+function utils.TearsUp(current, amount)
+    local currentTears = 30 / (current + 1)
+    local newTears = currentTears + amount
     return math.max((30 / newTears) - 1, -0.9999)
 end
 
