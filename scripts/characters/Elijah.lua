67a364353c863bb1ce720bcbaa50e9e50a5f9563
cryoxe <71180076+cryoxe@users.noreply.github.com>
2025-11-16 03:50:26 +0100

    refactor(Elijah): Added MAGIC NUMBERSs + funcs tables

--- scripts/characters/Elijah.lua
+++ scripts/characters/Elijah.lua
@@ -1,6 +1,24 @@
 local mod = DiesIraeMod
 local game = Game()
+local sfx = SFXManager()
 
+---@class Utils
+local utils = include("scripts.core.utils")
+
+local save = mod.SaveManager
+
+
+--- MAGIC NUMBERS
+---
+
+ELIJAHSWILL_HEADER = "Elijah's Will"
+
+WILL_SPEED_UP = 0.1
+WILL_TEARS_UP = 0.15
+WILL_DAMAGE_UP = 0.2
+WILL_RANGE_UP = 0.25
+WILL_SHOT_SPEED_UP = 0.1
+WILL_LUCK_UP = 0.5
 
 --- Definitions
 ---
@@ -27,6 +45,59 @@ local spawnElijahWill = {
     [PickupVariant.PICKUP_KEY] = elijahWill,
     [PickupVariant.PICKUP_BOMB] = elijahWill,
     [PickupVariant.PICKUP_COIN] = elijahWill,
+}
+
+---@alias statUpFun fun(data: table): string
+---@type statUpFun[]
+local statsUpFuncs = {
+    function(data)
+        data.WillSpeed = (data.WillSpeed or 0) + WILL_SPEED_UP
+        print(data.WillSpeed)
+        return "Speed Up!"
+    end,
+    function(data)
+        data.WillFireDelay = (data.WillFireDelay or 0) + WILL_TEARS_UP
+        return "Tears Up!"
+    end,
+    function(data)
+        data.WillDamage = (data.WillDamage or 0) + WILL_DAMAGE_UP
+        return "Damage Up!"
+    end,
+    function(data)
+        data.WillRange = (data.WillRange or 0) + WILL_RANGE_UP
+        return "Range Up!"
+    end,
+    function(data)
+        data.WillShotSpeed = (data.WillShotSpeed or 0) + WILL_SHOT_SPEED_UP
+        return "Shot Speed Up!"
+    end,
+    function(data)
+        data.WillLuck = (data.WillLuck or 0) + WILL_LUCK_UP
+        return "Luck Up!"
+    end,
+}
+
+---@alias cacheFun fun(player: EntityPlayer)
+---@type table<CacheFlag, cacheFun>
+local cacheFuncs = {
+    [CacheFlag.CACHE_SPEED] = function(player)
+        player.MoveSpeed = player.MoveSpeed + (save.GetRunSave(player).WillSpeed or 0)
+    end,
+    [CacheFlag.CACHE_FIREDELAY] = function(player)
+        player.MaxFireDelay = utils.TearsUp(player.MaxFireDelay, save.GetRunSave(player).WillFireDelay or 0)
+    end,
+    [CacheFlag.CACHE_DAMAGE] = function(player)
+        player.Damage = player.Damage + (save.GetRunSave(player).WillDamage or 0)
+    end,
+    [CacheFlag.CACHE_RANGE] = function(player)
+        player.TearRange = player.TearRange + 40 * (save.GetRunSave(player).WillRange or 0)
+    end,
+    [CacheFlag.CACHE_SHOTSPEED] = function(player)
+        player.ShotSpeed = player.ShotSpeed + (save.GetRunSave(player).WillShotSpeed or 0)
+    end,
+    [CacheFlag.CACHE_LUCK] = function(player)
+        player.Luck = player.Luck + (save.GetRunSave(player).WillLuck or 0)
+    end,
 }
 
 local elijahFuncs = {}
@@ -61,44 +132,22 @@ mod:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, elijahFuncs.OnPickupInit, elij
 ---Display a header when picking up a Elijah's Will
 ---@param pickup EntityPickup
 ---@param collider Entity
----@return boolean
+---@return boolean | nil
 function elijahFuncs:OnPickupCollision(pickup, collider)
     local player = collider:ToPlayer()
     if not player or player:GetPlayerType() ~= elijah then return false end
 
-    local data = player:GetData()
-    data.ElijahBoosts = data.ElijahBoosts or {}
+    local data = save.GetRunSave(player)
 
-    local stat = math.random(6)
-    local boostMessage = ""
+    local stat = math.random(#statsUpFuncs)
+    StatUp = statsUpFuncs[stat]
+    local secondaryString = StatUp(data)
 
-    if stat == 1 then
-        data.ElijahBoosts.Damage = (data.ElijahBoosts.Damage or 0) + 0.1
-        boostMessage = "Damage Up!"
-    elseif stat == 2 then
-        data.ElijahBoosts.Tears = (data.ElijahBoosts.Tears or 0) + 0.1
-        boostMessage = "Tears Up!"
-    elseif stat == 3 then
-        data.ElijahBoosts.Speed = (data.ElijahBoosts.Speed or 0) + 0.1
-        boostMessage = "Speed Up!"
-    elseif stat == 4 then
-        data.ElijahBoosts.Range = (data.ElijahBoosts.Range or 0) + 0.1
-        boostMessage = "Range Up!"
-    elseif stat == 5 then
-        data.ElijahBoosts.Luck = (data.ElijahBoosts.Luck or 0) + 0.1
-        boostMessage = "Luck Up!"
-    elseif stat == 6 then
-        data.ElijahBoosts.ShotSpeed = (data.ElijahBoosts.ShotSpeed or 0) + 0.1
-        boostMessage = "Shot Speed Up!"
-    end
+    player:AddCacheFlags(CacheFlag.CACHE_ALL, true)
 
-    player:AddCacheFlags(CacheFlag.CACHE_ALL)
-    player:EvaluateItems()
-
-    Game():GetHUD():ShowItemText("Elijah's Will", boostMessage)
-    SFXManager():Play(SoundEffect.SOUND_POWERUP1)
+    game:GetHUD():ShowItemText(ELIJAHSWILL_HEADER, secondaryString)
+    sfx:Play(SoundEffect.SOUND_POWERUP1)
     pickup:Remove()
-    return true
 end
 
 mod:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, elijahFuncs.OnPickupCollision, elijahWill)
@@ -110,25 +159,9 @@ mod:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, elijahFuncs.OnPickupCollis
 function elijahFuncs:EvaluateCache(player, cacheFlag)
     if player:GetPlayerType() ~= elijah then return end
 
-    local data = player:GetData()
-    if not data.ElijahBoosts then return end
-
-    if cacheFlag == CacheFlag.CACHE_DAMAGE then
-        player.Damage = player.Damage + (data.ElijahBoosts.Damage or 0)
-    elseif cacheFlag == CacheFlag.CACHE_FIREDELAY then
-        local tearsUp = data.ElijahBoosts.Tears or 0
-        local currentTears = 30 / (player.MaxFireDelay + 1)
-        local newTears = currentTears + tearsUp
-        player.MaxFireDelay = math.max(1, (30 / newTears) - 1)
-    elseif cacheFlag == CacheFlag.CACHE_SPEED then
-        player.MoveSpeed = player.MoveSpeed + (data.ElijahBoosts.Speed or 0)
-    elseif cacheFlag == CacheFlag.CACHE_RANGE then
-        player.TearRange = player.TearRange + (data.ElijahBoosts.Range or 0)
-    elseif cacheFlag == CacheFlag.CACHE_LUCK then
-        player.Luck = player.Luck + (data.ElijahBoosts.Luck or 0)
-    elseif cacheFlag == CacheFlag.CACHE_SHOTSPEED then
-        player.ShotSpeed = player.ShotSpeed + (data.ElijahBoosts.ShotSpeed or 0)
-    end
+    local CacheFunc = cacheFuncs[cacheFlag]
+    if (not CacheFunc) then return end
+    CacheFunc(player)
 end
 
 mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, elijahFuncs.EvaluateCache)
@@ -137,16 +170,15 @@ mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, elijahFuncs.EvaluateCache)
 ---Replace base pickup with the Elijah's Will.
 ---@param type EntityType
 ---@param variant integer
----@param subtype integer
 ---@param seed integer
 ---@return table | nil
-function elijahFuncs:PreEntitySpawnWill(type, variant, subtype, _, _, _, seed)
+function elijahFuncs:PreEntitySpawnWill(type, variant, _, _, _, _, seed)
     if type ~= EntityType.ENTITY_PICKUP then return end
     if not PlayerManager.AnyoneIsPlayerType(elijah) then return end
 
     local will = spawnElijahWill[variant]
     if will then
-        return { type, will, subtype, seed }
+        return { type, will, 0, seed }
     end
 end
 
