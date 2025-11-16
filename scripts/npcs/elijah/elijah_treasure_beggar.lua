661355764df4518e94f599ccda1a8090cbbcf6d0
cryoxe <71180076+cryoxe@users.noreply.github.com>
2025-11-16 17:43:25 +0100

    feat(beggar): Added treasure beggar

---
+++ scripts/npcs/elijah/elijah_treasure_beggar.lua
@@ -0,0 +1,49 @@
+local mod = DiesIraeMod
+local game = Game()
+local sfx = SFXManager()
+
+---@class BeggarUtils
+local beggarUtils = include("scripts.npcs.elijah.elijah_utils_beggar")
+
+--- MAGIC NUMBERS
+---
+
+local BASE_REWARD_CHANCES = 0.33 -- default is 1/3, if that's the case this insn't even needed
+local BEGGAR_ITEM_POOL = ItemPoolType.POOL_TREASURE
+
+--- Definitions
+---
+
+local beggar = mod.ElijahNPCs.BeggarElijah
+
+local beggarFuncs = {}
+
+
+--- Callbacks
+---
+
+---@param beggarEntity EntityNPC
+---@param collider Entity
+---@param _ any
+function beggarFuncs:PostSlotCollision(beggarEntity, collider, _)
+    local player = collider:ToPlayer()
+    if not player then return end
+
+    local _ = beggarUtils.OnBeggarCollision(beggarEntity, player, BASE_REWARD_CHANCES)
+end
+
+mod:AddCallback(ModCallbacks.MC_POST_SLOT_COLLISION, beggarFuncs.PostSlotCollision, beggar)
+
+
+---@param beggarEntity EntityNPC
+function beggarFuncs.PostSlotUpdate(beggarEntity)
+    beggarUtils.StateMachine(beggarEntity, BEGGAR_ITEM_POOL)
+end
+mod:AddCallback(ModCallbacks.MC_POST_SLOT_UPDATE, beggarFuncs.PostSlotUpdate, beggar)
+
+
+---@param beggarEntity EntityNPC
+function beggarFuncs.PreSlotExplosion(beggarEntity)
+    beggarUtils.DoBeggarExplosion(beggarEntity)
+end
+mod:AddCallback(ModCallbacks.MC_PRE_SLOT_CREATE_EXPLOSION_DROPS, beggarFuncs.PreSlotExplosion, beggar)
