8923ed880a5b924b9ea80aec77862d04ea1a690d
cryoxe <71180076+cryoxe@users.noreply.github.com>
2025-11-18 15:14:43 +0100

    feat(utils): added function to choose from weighted able

--- scripts/core/utils.lua
+++ scripts/core/utils.lua
@@ -18,4 +18,26 @@ function utils.TearsUp(current, amount)
     return math.max((30 / newTears) - 1, -0.9999)
 end
 
+
+---Choose a random element from a weighted table
+---@generic T
+---@alias weight_table table<integer, T>
+---@param table weight_table
+---@param rng RNG
+---@return T
+function utils.WeightedRandom(table, rng)
+    local size = 0
+    for _,pair in ipairs(table) do
+        size = size + pair[1]
+    end
+
+    local select = rng:RandomInt(size) + 1
+    for _,pair in ipairs(table) do
+        select = select - pair[1]
+        if (select <= 0) then return pair[2] end
+    end
+
+    return nil
+end
+
 return utils
