3633589664a289c5f863d61782c8a0d9ad553085
cryoxe <71180076+cryoxe@users.noreply.github.com>
2025-11-19 15:57:53 +0100

    refactor(beggar): refactor BATTERY beggar

--- scripts/npcs/elijah/elijah_battery_beggar.lua
+++ scripts/npcs/elijah/elijah_battery_beggar.lua
@@ -1,111 +1,88 @@
 local mod = DiesIraeMod
 local game = Game()
 local sfx = SFXManager()
+
+---@class BeggarUtils
+local beggarUtils = include("scripts.npcs.elijah.elijah_utils_beggar")
+
+--- MAGIC NUMBERS
+---
+
+local BASE_REWARD_CHANCES = 0.25
+local BEGGAR_ITEM_POOL = ItemPoolType.POOL_BATTERY_BUM
+local BEGGAR_PICKUP = PickupVariant.PICKUP_LIL_BATTERY
+
+--- Definitions
+---
+
 local beggar = mod.Entities.BEGGAR_BatteryElijah.Var
 
--- Payout chances
-local chanceCharge = 0.8125
-local chanceBattery = 0.10
-local chanceItem = 0.0625
-local chanceTrinketAAA = 0.0125
-local chanceTrinketWatch = 0.0125
+---@type beggarEventPool
+local beggarEvents = {
+    {
+        1,
+        function(beggarEntity)
+            beggarUtils.SpawnItemFromPool(beggarEntity, BEGGAR_ITEM_POOL)
+            return true
+        end
+    },
+    {
+        5,
+        function(beggarEntity)
+            beggarUtils.SpawnTrinket(beggarEntity, TrinketType.TRINKET_WATCH_BATTERY)
+            return true
+        end
+    },
+        {
+        5,
+        function(beggarEntity)
+            beggarUtils.SpawnTrinket(beggarEntity, TrinketType.TRINKET_AAA_BATTERY)
+            return true
+        end
+    },
+    {
+        20,
+        function(beggarEntity)
+            beggarUtils.SpawnPickup(beggarEntity, BEGGAR_PICKUP)
+            return false
+        end
+    }
+}
 
-function mod:BatteryBeggarCollision(beggarEntity, collider, low)
-    if not collider:ToPlayer() then return end
+local beggarFuncs = {}
+
+
+--- Callbacks
+---
+
+---@param beggarEntity EntityNPC
+---@param collider Entity
+---@param _ any
+function beggarFuncs:PostSlotCollision(beggarEntity, collider, _)
     local player = collider:ToPlayer()
-    local sprite = beggarEntity:GetSprite()
-
-    if player:GetPlayerType() ~= mod.Players.Elijah then
-        return
-    end
-
-    if sprite:IsPlaying("Idle") then
-        local rng = beggarEntity:GetDropRNG()
-        local paid = mod:DrainElijahStat(player)
-
-        if paid then
-            sfx:Play(SoundEffect.SOUND_SCAMPER)
-            sprite:Play("PayPrize")
-            beggarEntity:GetData().LastPayer = player
-        end
+    if not player then return end
+    print("TOUCHE")
+    local ok = beggarUtils.OnBeggarCollision(beggarEntity, player, BASE_REWARD_CHANCES)
+    if ok then
+        player:PlayExtraAnimation("Sad")
     end
 end
-mod:AddCallback(ModCallbacks.MC_POST_SLOT_COLLISION, mod.BatteryBeggarCollision, beggar)
+
+mod:AddCallback(ModCallbacks.MC_POST_SLOT_COLLISION, beggarFuncs.PostSlotCollision, beggar)
 
 
-function mod:BatteryBeggarUpdate(beggarEntity)
-    local sprite = beggarEntity:GetSprite()
-    local rng = beggarEntity:GetDropRNG()
-    local data = beggarEntity:GetData()
-
-    if sprite:IsFinished("PayNothing") then
-        sprite:Play("Idle")
-    elseif sprite:IsFinished("PayPrize") then
-        sprite:Play("Prize")
-    elseif sprite:IsFinished("Prize") then
-        local player = data.LastPayer or Isaac.GetPlayer(0)
-        local roll = rng:RandomFloat()
-        if roll < chanceCharge then
-            local charges = rng:RandomInt(3) + 1
-            local totalAdded = 0
-
-            if player:GetActiveItem(ActiveSlot.SLOT_PRIMARY) > 0 then
-                totalAdded = totalAdded + player:AddActiveCharge(charges, ActiveSlot.SLOT_PRIMARY, true, true)
-            end
-            if player:GetActiveItem(ActiveSlot.SLOT_POCKET) > 0 then
-                totalAdded = totalAdded + player:AddActiveCharge(charges, ActiveSlot.SLOT_POCKET, true, true)
-            end
-
-            if totalAdded > 0 then
-                sfx:Play(SoundEffect.SOUND_BATTERYCHARGE)
-                Game():GetHUD():ShowItemText("Battery Beggar", "+" .. tostring(totalAdded) .. " Charge")
-                Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, beggarEntity.Position, Vector.Zero, nil)
-            else
-                Game():GetHUD():ShowItemText("Battery Beggar", "No Active Item!")
-            end
-            sprite:Play("Idle")
-
-        elseif roll < chanceCharge + chanceBattery then
-            Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, 1,
-                Isaac.GetFreeNearPosition(beggarEntity.Position, 40), Vector.Zero, nil)
-            sfx:Play(SoundEffect.SOUND_BATTERYCHARGE)
-            sprite:Play("Idle")
-
-        elseif roll < chanceCharge + chanceBattery + chanceItem then
-            sfx:Play(SoundEffect.SOUND_SLOTSPAWN)
-            local item = game:GetItemPool():GetCollectible(ItemPoolType.POOL_BATTERY_BUM, true)
-            Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, item,
-                Isaac.GetFreeNearPosition(beggarEntity.Position, 40), Vector.Zero, nil)
-            sprite:Play("Teleport")
-
-        elseif roll < chanceCharge + chanceBattery + chanceItem + chanceTrinketAAA then
-            Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_TRINKET, TrinketType.TRINKET_AAA_BATTERY,
-                Isaac.GetFreeNearPosition(beggarEntity.Position, 40), Vector.Zero, nil)
-            sprite:Play("Teleport")
-        else
-            Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_TRINKET, TrinketType.TRINKET_WATCH_BATTERY,
-                Isaac.GetFreeNearPosition(beggarEntity.Position, 40), Vector.Zero, nil)
-            sprite:Play("Teleport")
-        end
-    end
-    if sprite:IsFinished("Teleport") then
-        sfx:Play(SoundEffect.SOUND_POOF01)
-        Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, beggarEntity.Position, Vector.Zero, nil)
-        beggarEntity:Remove()
-    end
+---@param beggarEntity EntityNPC
+function beggarFuncs:PostSlotUpdate(beggarEntity)
+    beggarUtils.StateMachine(beggarEntity, beggarEvents)
 end
-mod:AddCallback(ModCallbacks.MC_POST_SLOT_UPDATE, mod.BatteryBeggarUpdate, beggar)
+
+mod:AddCallback(ModCallbacks.MC_POST_SLOT_UPDATE, beggarFuncs.PostSlotUpdate, beggar)
 
 
-function mod:BatteryBeggarExploded(beggar)
-    for i = 1, 2 do
-        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, 1,
-            beggar.Position + RandomVector() * 20, RandomVector() * 2, beggar)
-    end
-    sfx:Play(SoundEffect.SOUND_MEATY_DEATHS)
-    game:SpawnParticles(beggar.Position, EffectVariant.BLOOD_EXPLOSION, 1, 3, Color(0.8, 1, 1, 1, 0, 0.5, 1))
-    Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.BLOOD_SPLAT, 0, beggar.Position, Vector.Zero, beggar)
-    beggar:Remove()
+---@param beggarEntity EntityNPC
+function beggarFuncs:PreSlotExplosion(beggarEntity)
+    beggarUtils.DoBeggarExplosion(beggarEntity)
     return false
 end
-mod:AddCallback(ModCallbacks.MC_PRE_SLOT_CREATE_EXPLOSION_DROPS, mod.BatteryBeggarExploded, beggar)
+
+mod:AddCallback(ModCallbacks.MC_PRE_SLOT_CREATE_EXPLOSION_DROPS, beggarFuncs.PreSlotExplosion, beggar)
