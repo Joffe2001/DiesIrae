e17f6a4758906fcf9d127e6311e98ef133e859f2
Jam0xT <netheritejam@gmail.com>
2025-11-27 14:21:49 +0800

    feat(item): stab wound change

--- scripts/items/passive/stabWound.lua
+++ scripts/items/passive/stabWound.lua
@@ -1,41 +1,27 @@
 local mod = DiesIraeMod
 
-local StabWound = {}
+local stabWound = {
+    FLAT_TEARS_UP = 0.5,
+}
 local game = Game()
 
-function StabWound:onCache(player, cacheFlag)
+---@param player EntityPlayer
+function stabWound:onCache(player, cacheFlag)
     if not player:HasCollectible(mod.Items.StabWound) then return end
 
-    if cacheFlag == CacheFlag.CACHE_DAMAGE then
-        player.Damage = player.Damage + 1.0
-
-    elseif cacheFlag == CacheFlag.CACHE_FIREDELAY then
+    local num = player:GetCollectibleNum(mod.Items.StabWound, false)
+    
+    if cacheFlag == CacheFlag.CACHE_FIREDELAY then
         local currentTears = 30 / (player.MaxFireDelay + 1)
-        local newTears = currentTears + 0.5
+        local newTears = currentTears + stabWound.FLAT_TEARS_UP * num
         player.MaxFireDelay = math.max(1, (30 / newTears) - 1)
     end
 end
+mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, stabWound.onCache)
 
-function StabWound:onUpdate()
-    for i = 0, Game():GetNumPlayers() - 1 do
-        local player = Isaac.GetPlayer(i)
-        local data = player:GetData()
-
-        if player:HasCollectible(mod.Items.StabWound) then
-            if not data.StabWoundCacheApplied then
-                player:AddCacheFlags(CacheFlag.CACHE_DAMAGE | CacheFlag.CACHE_FIREDELAY)
-                player:EvaluateItems()
-                data.StabWoundCacheApplied = true
-            end
-        else
-            if data.StabWoundCacheApplied then
-                data.StabWoundCacheApplied = nil
-                player:AddCacheFlags(CacheFlag.CACHE_DAMAGE | CacheFlag.CACHE_FIREDELAY)
-                player:EvaluateItems()
-            end
-        end
-    end
+---@param player EntityPlayer
+function stabWound:onPostAddCollectible(collectibleType, charge, firstTime, slot, varData, player)
+    player:AddCacheFlags(CacheFlag.CACHE_DAMAGE | CacheFlag.CACHE_FIREDELAY, true)
+    player:AddCollectible(CollectibleType.COLLECTIBLE_STEVEN, 0, false)
 end
-
-mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, StabWound.onCache)
-mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, StabWound.onUpdate)
+mod:AddCallback(ModCallbacks.MC_POST_ADD_COLLECTIBLE, stabWound.onPostAddCollectible, mod.Items.StabWound)
